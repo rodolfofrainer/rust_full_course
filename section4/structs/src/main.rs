bf06d1c125c5b89f3c22514eeb4b292e03c09855
Rodolfo Frainer <91473305+rodolfofrainer@users.noreply.github.com>
2023-04-22 01:28:20 +0100

    section 4 - lifetimes

--- section4/structs/src/main.rs
+++ section4/structs/src/main.rs
@@ -9,23 +9,23 @@
 // struct UnitStruct;
 
 
-struct Square{
-    width:u32,
-    height:u32,
-}
+// struct Square{
+//     width:u32,
+//     height:u32,
+// }
 
-impl Square{
-    fn area(&self)->u32{
-        self.width*self.height
-    }
-    fn whats_my_width(&self)->u32{
-        self.width
-    }
+// impl Square{
+//     fn area(&self)->u32{
+//         self.width*self.height
+//     }
+//     fn whats_my_width(&self)->u32{
+//         self.width
+//     }
 
-    fn change_width(&mut self, new_width: u32){
-        self.width = new_width;
-    }
-}
+//     fn change_width(&mut self, new_width: u32){
+//         self.width = new_width;
+//     }
+// }
 
 fn main() {
 //     let user1 = User{active:true, username: String::from("Rf"), sign_in_count: 12};
@@ -41,16 +41,35 @@ fn main() {
 //     //1..5 UnitStruct  .. = Range -------- Range(start:1,end:5)
 // }
 
+// let mut square = Square{width: 5, height: 5};
+    // println!("{}", square.area());
+    // println!("{}", square.whats_my_width());
+    // square.change_width(12);
+    // println!("{}", square.whats_my_width());
+
+
+
+
+    let r;
+    {
+        let x=5;
+        r = &x;
+    }// x is dropped here
+    println!("{}", r); //borrow used(no value)
+
+    //i32
+    //&'a i32
+    //&'a mut i32
+
+    fn example<'a, 'b>(x: &'a str, y: &'b str) -> 'a str{
+        x
+    } //'a for a parameter, 'b for a second parameter
+}
+
+
 // fn build_user(username:String)->User{
 //     User{
 //         username,
 //         active:true,
 //         sign_in_count:1,
 //     }
-
-    let mut square = Square{width: 5, height: 5};
-    println!("{}", square.area());
-    println!("{}", square.whats_my_width());
-    square.change_width(12);
-    println!("{}", square.whats_my_width());
-}
